ff56a9ae84f61ca1f885f6e01abfd115cfe26ca6
alohajt <alohajiatao@gmail.com>
2020-06-30 21:18:50 -0600

    login page works

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -9,14 +9,30 @@ class AuthenticationController < ApplicationController
                 token = JWT.encode(payload, secret)
                 render json: {token: token}
             else 
-                render json: "wrong password/username!"
-                # render json: "wrong password/username!", status: :unanthorized
+                render json: "wrong password/username!", status: :unauthorized
                 
             end
        else 
-        render json: "no such user!"
-        # render json: "no such user!", status: :unanthorized
+        render json: "no such user!", status: :unauthorized
        end
     end
+
+    def register 
+        @user =  User.find_by(username: params[:username])
+ 
+        if @user 
+             if @user.authenticate(params[:password])
+                 payload = {  user_id: @user.id }
+                 secret = Rails.application.secrets.secret_key_base
+                 token = JWT.encode(payload, secret)
+                 render json: {token: token}
+             else 
+                 render json: "wrong password/username!", status: :unauthorized
+                 
+             end
+        else 
+         render json: "no such user!", status: :unauthorized
+        end
+     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create]
 
   post "/login", to: "authentication#login"
+  post "/register", to: "authentication#register"
 end
